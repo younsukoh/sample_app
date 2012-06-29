b2862f79698f54820b32c48e7b46c25c2be0c2e5
Younsuk Oh <oys0317@nate.com>
2012-06-29 16:49:26 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+
+  #get "users/new"
 
   get "static_pages/home"
   #match '/', to: 'static_pages#home'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name      "Younsuk Oh"
+		email     "oys0317@nate.com"
+		password  "foobar"
+		password_confirmation "foobar"
+	end
+end
