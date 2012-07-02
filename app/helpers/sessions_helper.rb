8519f0a1b8a5103d7c3217038d5cc465563599b4
younsukoh <oys0317@nate.com>
2012-07-03 07:18:48 +0900

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,7 +19,14 @@ module SessionsHelper
 
   	def current_user?(user)
     	user == current_user
-	 end
+	end
+
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path, notice: "Please sign in."
+		end
+	end
 
 	def sign_out
 		self.current_user = nil
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -45,9 +45,7 @@ describe "User Pages" do
         end
 
         it { should have_link('delete', href: user_path(User.first)) }
-        it "should be able to delete another user" do
-          expect { click_link('delete') }.to change(User, :count).by(-1)
-        end
+        
         it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
@@ -67,27 +65,6 @@ describe "User Pages" do
         before { click_button submit }
         it { should have_selector('h1', text: 'Sign up')}
         it { should have_content('error') }
-      end
-  	end
-
-  	describe "with valid information" do
-  		before do
-  			fill_in "Name", with: "Example User"
-  			fill_in "Email", with: "user@example.com"
-  			fill_in "Password", with: "foobar"
-  			fill_in "Confirmation", with: "foobar"
-  		end
-
-  		it "should create a user" do
-  			expect { click_button submit }.to change(User, :count).by(1)
-  		end
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by_email('user@example.com') }
-
-        it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome')}
-        it { should have_link('Sign out') }
       end
   	end
   end
@@ -153,4 +130,22 @@ describe "User Pages" do
       end
     end
   end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
+
 end
